d231b3dc1cb68d398a39790cf3fc1cb715607a5b
NAHO <ohmap@naver.com>
2026-01-10 21:54:44 +0900

    feat: JWT 인터셉터 구현 및 WebMvc 공통 설정 추가

---
+++ Hackathon-team5/src/main/java/com/example/demo/global/security/interceptor/JwtInterceptor.java
@@ -0,0 +1,40 @@
+package com.example.demo.global.security.interceptor;
+
+import com.example.demo.global.exception.BusinessException;
+import com.example.demo.global.exception.ErrorCode;
+import com.example.demo.global.security.JwtTokenProvider;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtInterceptor implements HandlerInterceptor {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object Handler) {
+
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Bearer ")){
+            throw new BusinessException(ErrorCode.AUTHENTICATION_REQUIRED);
+        }
+
+        // 'Bearer '를 제외한 실제 토큰 값만 추출
+        String token = header.substring(7);
+
+        if(!jwtTokenProvider.validateToken(token)) {
+            throw new BusinessException(ErrorCode.INVALID_TOKEN);
+        }
+
+        String userId = jwtTokenProvider.getPayload(token);
+        request.setAttribute("userId", Long.parseLong(userId));
+
+        return true;
+
+    }
+}
